bda0b09ce9f7cf3a4e77c616f05cc9393f8496ea
Colin Huber <cbhuber@gmail.com>
2023-07-18 22:26:55 -0600

    feat: icons and share quote.

--- background.js
+++ background.js
@@ -12,11 +12,30 @@ function myImageClick(info, tab) {
   });
 }
 
+function myQuoteClick(info, tab) {
+  console.log("Clicked an image", info, tab);
+  chrome.windows.create({
+    url:
+      "https://www.facebook.com/sharer.php?u=" +
+      info.pageUrl +
+      "&display=popup&quote=" +
+      info.selectionText,
+    type: "popup",
+  });
+}
+
 // Right click added option
 chrome.contextMenus.create({
   title: "Share Image",
   contexts: ["image"],
   onclick: myImageClick,
+});
+
+// Right click added option
+chrome.contextMenus.create({
+  title: "Share Quote",
+  contexts: ["selection"], // Text selection; hold left mouse button down & select text
+  onclick: myQuoteClick,
 });
 
 // Allow this background script to receive messages from index.js
